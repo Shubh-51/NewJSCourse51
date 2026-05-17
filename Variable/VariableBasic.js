8402f2c37a17ce46ea49becee6aa0f9666d2f54e
Shubh-51 <shubhmaurya73536@gmail.com>
2026-05-17 10:59:49 +0530

    variable1

--- Variable/VariableBasic.js
+++ Variable/VariableBasic.js
@@ -14,8 +14,20 @@ accountPassword="5678";
 accountCity="Delhi";
 console.log(accountId);
 
-/* Perfer not two use var
+/* Perfer not to use "var"
 because of block scope and function scope issues.
 */
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);    
+
+
+//IN javascript we have 3 types of variable declaration keywords: var, let and const.
+
+//var is function scoped and can be re-declared and updated.
+//let is block scoped and can be updated but not re-declared.
+//const is block scoped and cannot be updated or re-declared.
+
+//In javascript we can declare a variable without using any keyword, but it is not recommended as it creates a global variable and can lead to unexpected behavior.
+
+
+//In javascript, we can declare a varible without initializing it, and it will have the value of undefined. We can also initialize a variable at the time of declaration or later in the code.
